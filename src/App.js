61229d1f866c81e3c62c4db2ac2a359dd095faab
Natasha Lawson <natasha.lawson93@gmail.com>
2020-03-22 15:58:50 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn React", id: 0, isComplete: false },
+    { text: "Draw", id: 1, isComplete: false },
+    { text: "Swim", id: 2, isComplete: false }
+  ]);
+  const [newTodo, setNewTodo] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <form onSubmit={e => {
+        e.preventDefault();
+        const nextId = todos[todos.length - 1].id + 1
+        setTodos([...todos, { text: newTodo, id: nextId, isComplete: false }])
+        setNewTodo("");
+      }}>
+        <input value={newTodo} required={true} onChange={e => {
+          setNewTodo(e.target.value)
+        }} />
+        <button>Add New Todo</button>
+      </form>
+      {todos.map((todo, i) => (
+        <div key={todo.id}>
+          <input type="checkbox" checked={todo.isComplete} onChange={e => {
+            const newTodos = Object.assign([], todos, {
+              [i]: { ...todo, isComplete: !todo.isComplete }
+            })
+            setTodos(newTodos)
+          }} />
+          <input
+            style={{
+              textDecoration: todo.isComplete ? "line-through" : "none"
+            }}
+            value={todo.text}
+            onChange={e => {
+              const newTodos = Object.assign([], todos, {
+                [i]: { ...todo, text: e.target.value }
+              })
+
+              setTodos(newTodos);
+
+            }}
+          />
+          <button onClick={e => {
+            const newTodos = todos.filter((todo, index) => index !== i)
+            setTodos(newTodos)
+          }}>X</button>
+        </div>
+
+      ))
+      }
+    </div >
   );
 }
 
